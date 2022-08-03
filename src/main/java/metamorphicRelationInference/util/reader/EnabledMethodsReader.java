3aa53f19fd01040543b3049bb249b56cf68012bc
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-03 17:05:20 -0300

    Add a specific class to read enabled methods file

---
+++ src/main/java/metamorphicRelationInference/util/reader/EnabledMethodsReader.java
@@ -0,0 +1,35 @@
+package metamorphicRelationInference.util.reader;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import metamorphicRelationInference.util.ReaderUtils;
+
+public class EnabledMethodsReader {
+
+  public static Map<String, Map<Method, Boolean>> readEnabledMethodsPerState(
+      Class<?> cut, String pathToFile) {
+    List<String> lines = ReaderUtils.getLines(pathToFile);
+    Map<String, Map<Method, Boolean>> enabledMethodsPerState = new HashMap<>();
+    for (String line : lines) {
+      String[] components = line.trim().split(" : ");
+      String state = components[0];
+      Method method;
+      try {
+        method = cut.getDeclaredMethod(components[1]);
+      } catch (NoSuchMethodException e) {
+        throw new RuntimeException(e);
+      }
+      boolean result = Boolean.parseBoolean(components[2]);
+
+      Map<Method, Boolean> methodsAndResults = enabledMethodsPerState.get(state);
+      if (methodsAndResults == null) {
+        methodsAndResults = new HashMap<>();
+      }
+      methodsAndResults.put(method, result);
+      enabledMethodsPerState.put(state, methodsAndResults);
+    }
+    return enabledMethodsPerState;
+  }
+}
