b822bed0797b074d1d3a1cbb2f6fbf8ceda10f0e
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-03 09:52:47 -0300

    Add a util class for read files

---
+++ src/main/java/metamorphicRelationInference/utils/ReaderUtils.java
@@ -0,0 +1,19 @@
+package metamorphicRelationInference.utils;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ReaderUtils {
+
+  public static List<String> getLines(String pathToFile) {
+    File file = new File(pathToFile);
+    try (BufferedReader br = Files.newBufferedReader(file.toPath(), Charset.defaultCharset())) {
+      return br.lines().collect(Collectors.toList());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
