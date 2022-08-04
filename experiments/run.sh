15c687f87fce8b60471269adf3024a75b0f92cb0
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-04 15:32:58 -0300

    Change the output size to 100

--- experiments/run.sh
+++ experiments/run.sh
@@ -6,4 +6,4 @@ subject_cp="$SUBJECTS_DIR/$subject_name/build/libs/*"
 
 input_file="experiments/$subject_name.properties"
 
-java -cp "$subject_cp:$JAR" randoop.main.Main gentests --testclass=com.example.myboundedstack.MyBoundedStack --output-limit=500 --literals-level=ALL --literals-file=literals/lits.txt
+java -cp "$subject_cp:$JAR" randoop.main.Main gentests --testclass=com.example.myboundedstack.MyBoundedStack --output-limit=100 --literals-level=ALL --literals-file=literals/lits.txt
