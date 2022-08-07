70499b4725543dbbe2f84ed8b81ad4930d0ff2ab
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-07 19:26:44 -0300

    Simplify validator

--- src/main/java/metamorphicRelationsInference/validator/Validator.java
+++ src/main/java/metamorphicRelationsInference/validator/Validator.java
@@ -6,19 +6,15 @@ import metamorphicRelationsInference.bag.Bag;
 import metamorphicRelationsInference.epa.EPAState;
 import metamorphicRelationsInference.metamorphicRelation.MetamorphicRelation;
 import metamorphicRelationsInference.util.Pair;
-import randoop.DummyVisitor;
 import randoop.generation.AbstractGenerator;
-import randoop.sequence.ExecutableSequence;
-import randoop.sequence.Sequence;
 import randoop.sequence.Variable;
-import randoop.test.DummyCheckGenerator;
 
 public class Validator {
 
-  private final AbstractGenerator explorer;
+  Executor executor;
 
   public Validator(AbstractGenerator explorer) {
-    this.explorer = explorer;
+    executor = new Executor(explorer);
   }
 
   public List<MetamorphicRelation> validate(
@@ -41,19 +37,12 @@ public class Validator {
             continue;
           }
           Variable var = pair.getFst();
-          Pair<Sequence, Sequence> sequences =
-              mr.createSequences(var.sequence, var.index, explorer);
-          ExecutableSequence seq1 = new ExecutableSequence(sequences.getFst());
-          ExecutableSequence seq2 = new ExecutableSequence(sequences.getSnd());
-          Object result1 = getObjectToCompare(seq1, mr.getVariablesToCompare().getFst());
-          Object result2 = getObjectToCompare(seq2, mr.getVariablesToCompare().getSnd());
-          System.out.println(result1.toString() + " - " + result2.toString());
-          System.out.println(seq1.sequence);
-          System.out.println(seq2.sequence);
-          System.out.println("**********************");
+          executor.setup(mr, var);
+          Object result1 = executor.getLeftResult();
+          Object result2 = executor.getRightResult();
           if (!result1.toString().equals(result2.toString())) {
             counterExampleFound = true;
-            mr.setCounterExample(new Pair<>(seq1.sequence, seq2.sequence));
+            mr.setCounterExample(executor.getSequences());
           }
         }
       }
@@ -65,13 +54,5 @@ public class Validator {
     }
     System.out.println("----------------------");
     return validMRs;
-  }
-
-  private Object getObjectToCompare(ExecutableSequence sequence, Variable var) {
-    sequence.execute(new DummyVisitor(), new DummyCheckGenerator());
-    Object[] values =
-        ExecutableSequence.getRuntimeValuesForVars(
-            Collections.singletonList(var), sequence.executionResults);
-    return values[0];
   }
 }
