0c21d37ed2f8289a3407e6d5d6d3e162949d356d
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-08 00:00:51 -0300

    Increase the output limit from 100 to 500. Now we use .properties files to read the class under test.

--- experiments/run.sh
+++ experiments/run.sh
@@ -4,6 +4,6 @@ subject_name=$1
 
 subject_cp="$SUBJECTS_DIR/$subject_name/build/libs/*"
 
-input_file="experiments/$subject_name.txt"
+input_file="experiments/$subject_name.properties"
 omit_methods_file="$OUTPUTS_DIR/$subject_name/pure-methods.txt"
-java -cp "$subject_cp:$JAR" randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=100 --literals-level=ALL --literals-file=literals/lits.txt
+java -cp "$subject_cp:$JAR" randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=500 --literals-level=ALL --literals-file=literals/lits.txt
