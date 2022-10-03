24a4b35db2e4668aa430f805ee36ac37c414d116
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-10-03 20:19:22 +0200

    Remove the valid mrs from the CSV summary

--- gen-summary.py
+++ gen-summary.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 import csv
 
-header = ['Class', 'MRs', 'Accept %', 'Reject %']
+header = ['Class', 'Input', 'Output', 'Accept %', 'Reject %']
 rows = []
 
 dir_path = 'output/'
@@ -13,20 +13,18 @@ for filename in os.listdir(dir_path):
             clazz = [line for line in lines if 'Class: ' in line][0].split(': ')[1].strip()
             accept_percentage = float([line for line in lines if '% of valid MRs: ' in line][0].split(': ')[1])
             reject_percentage = float([line for line in lines if '% of invalid MRs: ' in line][0].split(': ')[1])
+            input1 = [line.strip() for line in lines if 'Input: ' in line][0].split(': ')[1].split(' ')[1]
+            if not input1:
+                input1 = 0
+            else:
+                input1 = int(input1)
+            output = [line.strip() for line in lines if 'Output: ' in line][0].split(': ')[1].split(' ')[0]
+            if not output:
+                output = 0
+            else:
+                output = int(output)
 
-            valid_mrs = []
-            copy = False
-            for line in lines:
-                if "Valid MRs: " in line:
-                    copy = True
-                    continue
-                elif "Input: " in line:
-                    copy = False
-                    continue
-                elif copy and line.strip():
-                    valid_mrs.append(line.strip())
-
-            rows.append([clazz, valid_mrs, accept_percentage, reject_percentage])
+            rows.append([clazz, input1, output, accept_percentage, reject_percentage])
 
 filename = "output/output.csv"
     
