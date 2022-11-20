112d0cd9e5b3e5cf0cc0093f78b2aef48e1afdb3
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-11-20 17:30:15 +0100

    Update experiments folder and scripts

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,9 +1,10 @@
 source experiments/init_env.sh
 
 subject_name=$1
+subject_set=$2
 
 subject_cp="$SUBJECTS_DIR/$subject_name/build/libs/*"
 
-input_file="experiments/$subject_name.properties"
+input_file="experiments/$subject_set-subjects/$subject_name.properties"
 omit_methods_file="$OUTPUTS_DIR/$subject_name/pure-methods.txt"
 java -cp "$subject_cp:$JAR" randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true
--- run-all-subjects.py
+++ run-all-subjects.py
@@ -1,16 +1,19 @@
 import os
 import subprocess
 import csv
+import sys
 
 outputs_dir = 'output'
 
-dir_path = 'experiments/'
+subject_set = sys.argv[1]
+
+dir_path = f'experiments/{subject_set}-subjects'
 i = 0
 for filename in os.listdir(dir_path):
     if '.properties' in filename:
         subject_name = filename.split('.properties')[0]
         print('Running: ' + subject_name)
-        result = subprocess.run(f'experiments/run.sh {subject_name}', shell=True, stdout=subprocess.PIPE)
+        result = subprocess.run(f'experiments/run.sh {subject_name} {subject_set}', shell=True, stdout=subprocess.PIPE)
         output = result.stdout.decode('utf-8')
         print(output)
         f = open(f'{outputs_dir}/{subject_name}.txt', 'w')
