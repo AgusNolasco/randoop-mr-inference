6db3876a480afa4b477c77708bb76bae43069b8b
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-01-10 22:57:04 -0300

    Format changes

--- src/main/java/metamorphicRelationsInference/util/AdditionalOptions.java
+++ src/main/java/metamorphicRelationsInference/util/AdditionalOptions.java
@@ -7,25 +7,24 @@ import metamorphicRelationsInference.metamorphicRelation.GenerationStrategy;
 @Parameters(separators = "=")
 public class AdditionalOptions {
 
-    @Parameter(names = "--gen-strategy")
-    private String strategy;
+  @Parameter(names = "--gen-strategy")
+  private String strategy;
 
-    @Parameter(names = "--mrs-to-fuzz")
-    private int mrsToFuzz = 0;
+  @Parameter(names = "--mrs-to-fuzz")
+  private int mrsToFuzz = 0;
 
-    @Parameter(names = "--allow-epa-loops", arity = 1)
-    private boolean allowEPALoops = false;
+  @Parameter(names = "--allow-epa-loops", arity = 1)
+  private boolean allowEPALoops = false;
 
-    public GenerationStrategy generationStrategy() {
-        return GenerationStrategy.valueOf(strategy);
-    }
+  public GenerationStrategy generationStrategy() {
+    return GenerationStrategy.valueOf(strategy);
+  }
 
-    public int mrsToFuzz() {
-        return mrsToFuzz;
-    }
+  public int mrsToFuzz() {
+    return mrsToFuzz;
+  }
 
-    public boolean isEPALoopsAllowed() {
-        return allowEPALoops;
-    }
-
+  public boolean isEPALoopsAllowed() {
+    return allowEPALoops;
+  }
 }
