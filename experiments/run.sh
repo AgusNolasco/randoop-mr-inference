f4442b99a534eaeb7f51dda9064d98f90af09d62
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-01-10 23:04:02 -0300

    Add the new parameters to the script

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,11 +1,14 @@
 source experiments/init_env.sh
 
-subject_name=$1
-subject_set=$2
+subject_set=$1
+subject_name=$2
+gen_strategy=$3
+mrs_to_fuzz=$4
+allow_epa_loops=$5
 
 subject_cp="$SUBJECTS_DIR/$subject_name/build/libs/*"
 
 input_file="experiments/$subject_set-subjects/$subject_name.properties"
-omit_methods_file="$OUTPUTS_DIR/$subject_name/pure-methods.txt"
+omit_methods_file="$OUTPUTS_DIR/$subject_name/methods-to-ignore.txt"
 #java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" -Xbootclasspath/a:${RANDOOP_DIR}/replacecall-4.3.1.jar -javaagent:${RANDOOP_DIR}/replacecall-4.3.1.jar randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true
-java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true
+java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" randoop.main.Main gentests --classlist=$input_file --omit-methods-file=$omit_methods_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true --gen-strategy=$gen_strategy --mrs-to-fuzz=$mrs_to_fuzz --allow-epa-loops=$allow_epa_loops
