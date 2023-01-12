7ebe089e96534a2c78ba9fa9747b6bb3dc1bc005
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-01-12 16:21:35 -0300

    Add a new script to run randoop over all fuzzed mrs (only makes sense for EPA_CONFORMING technique)

---
+++ run-all-subjects-over-fuzzed-mrs.py
@@ -0,0 +1,19 @@
+import os
+import subprocess
+import csv
+import sys
+
+outputs_dir = 'output'
+
+subject_set   = sys.argv[1]
+gen_strategy  = sys.argv[2]
+mrs_to_fuzz   = sys.argv[3]
+allow_epa_loops = sys.argv[4]
+
+dir_path = f'experiments/{subject_set}-subjects'
+i = 0
+for filename in os.listdir(dir_path):
+    if '.properties' in filename:
+        subject_name = filename.split('.properties')[0]
+        print('Running: ' + subject_name)
+        result = subprocess.run(f'experiments/run-over-fuzzed-mrs.sh {subject_set} {subject_name} {gen_strategy} {mrs_to_fuzz} {allow_epa_loops}', shell=True, stdout=subprocess.PIPE)
