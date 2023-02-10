a1c64695113fbee8765b6a1d316cb5a212c41d0b
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-02-10 16:09:24 -0300

    Update run mutation over sbes mrs

--- experiments/run-sbes-checker.sh
+++ experiments/run-sbes-checker.sh
@@ -12,4 +12,28 @@ subject_cp="$SUBJECTS_DIR/$subject_name/build/libs/*"
 
 input_file="experiments/$subject_set-subjects/$subject_name.properties"
 
-java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" -Xbootclasspath/a:${RANDOOP_DIR}/replacecall-4.3.1.jar -javaagent:${RANDOOP_DIR}/replacecall-4.3.1.jar randoop.main.Main gentests --classlist=$input_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true --SBES
+mkdir -p "$SUBJECTS_DIR/$subject_name/mutants/original-class/"
+
+orig_class_path=$(find $SUBJECTS_DIR/$subject_name/src/main/java/ -type f -name "$subject_name.java")
+mutated_class_path=$(find $SUBJECTS_DIR/$subject_name/mutants/$mutant_number/ -type f -name "$subject_name.java")
+
+cp $orig_class_path $SUBJECTS_DIR/$subject_name/mutants/original-class/$subject_name.java
+
+cp $mutated_class_path $orig_class_path
+
+CURR_DIR=$PWD
+
+cd "$SUBJECTS_DIR/$subject_name/"
+./gradlew jar
+mutant_compiles=$?
+cd $CURR_DIR
+
+if [ $mutant_compiles -eq 0 ]; then
+    java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" -Xbootclasspath/a:${RANDOOP_DIR}/replacecall-4.3.1.jar -javaagent:${RANDOOP_DIR}/replacecall-4.3.1.jar randoop.main.Main gentests --classlist=$input_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true --SBES
+fi
+
+cp $SUBJECTS_DIR/$subject_name/mutants/original-class/$subject_name.java $orig_class_path
+
+cd "$SUBJECTS_DIR/$subject_name/"
+./gradlew jar
+cd $CURR_DIR
