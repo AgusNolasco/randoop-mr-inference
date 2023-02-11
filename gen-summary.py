c22deb0597a494ca3b58876b46c76449c2255ae1
Isaias <ibattaglia@dc.exa.unrc.edu.ar>
2023-02-11 20:46:18 -0300

    Update gen summary

--- gen-summary.py
+++ gen-summary.py
@@ -9,11 +9,17 @@ rows = []
 gen_strategy = sys.argv[1]
 mrs_to_fuzz = sys.argv[2]
 allow_epa_loops = sys.argv[3]
+random = False
+if len(sys.argv) == 5 and sys.argv[4] == 'random':
+    random = True
 
 dir_path = 'output'
 for subject_dir in os.listdir(dir_path):
     if os.path.isdir(f'{dir_path}/{subject_dir}'):
-        with open(f'{dir_path}/{subject_dir}/allow_epa_loops_{allow_epa_loops}/{gen_strategy}/{mrs_to_fuzz}/log.txt') as f:
+        path_to_output_dir = f'{dir_path}/{subject_dir}/allow_epa_loops_{allow_epa_loops}/{gen_strategy}/{mrs_to_fuzz}/'
+        if random:
+            path_to_output_dir += 'random/'
+        with open(path_to_output_dir + 'log.txt') as f:
             lines = f.readlines()
             clazz = [line for line in lines if 'Class: ' in line][0].split(': ')[1].strip()
             accept_percentage = float([line for line in lines if '% of valid MRs: ' in line][0].split(': ')[1])
@@ -28,7 +34,10 @@ for subject_dir in os.listdir(dir_path):
 
             rows.append([clazz, input1, output, accept_percentage, reject_percentage])
 
-filename = f"output/{gen_strategy}-{mrs_to_fuzz}-{allow_epa_loops}-resume.csv"
+if random:
+    filename = f"output/{gen_strategy}-{mrs_to_fuzz}-{allow_epa_loops}-random-resume.csv"
+else:
+    filename = f"output/{gen_strategy}-{mrs_to_fuzz}-{allow_epa_loops}-resume.csv"
     
 with open(filename, 'w') as csvfile: 
     csvwriter = csv.writer(csvfile) 
