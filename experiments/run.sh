8c306379b6912e20985061525ddefabf0efa95f2
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2023-04-16 21:54:33 -0300

    Update run scripts

--- experiments/run.sh
+++ experiments/run.sh
@@ -33,4 +33,5 @@ cd "$SUBJECTS_DIR/$subject_name/"
 ./gradlew jar
 cd $CURR_DIR
 
+echo "Running $subject_name"
 /usr/bin/time -f"%e" java -cp "$subject_cp:$RANDOOP_DIR/randoop-all-4.3.1.jar" -Xbootclasspath/a:${RANDOOP_DIR}/replacecall-4.3.1.jar -javaagent:${RANDOOP_DIR}/replacecall-4.3.1.jar randoop.main.Main gentests --classlist=$input_file --output-limit=2000 --time-limit=0 --literals-level=ALL --literals-file=literals/lits.txt --deterministic=true --forbid-null=true --gen-strategy=$gen_strategy --mrs-to-fuzz=$mrs_to_fuzz --allow-epa-loops=$allow_epa_loops > output/$subject_name/allow_epa_loops_$allow_epa_loops/$gen_strategy/$mrs_to_fuzz/log.txt
